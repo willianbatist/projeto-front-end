502b3786a88c71b27fc86338f9e3d912b0901cf9
Willian <willian.alves.b15@gmail.com>
2023-05-01 19:13:04 -0300

    styles(header): styles header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -2,13 +2,35 @@
 import Image from "next/image";
 import { HeaderStyled } from "./header.styled";
 import { increment } from "../../store/features/changeComponentsSlice";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import logo from "../../../../img/loog.png"
+import { useRef } from "react";
+
+interface Props {
+  dispatchEvent: string
+  statusComponent: string
+}
+
+function ItemNav({statusComponent, dispatchEvent = "processes"}: Props) {
+  const change = useAppSelector((state) => state.changeComponents.value) || "processes";
+  const dispatch = useAppDispatch();
+  
+
+  const setStyled = () => {
+    const className = "componentSeleted";
+    return dispatchEvent === change && className;
+  };
+
+  console.log(setStyled());
+  
+
+  return(
+    <li className={`${setStyled()}`} onClick={() => dispatch(increment(dispatchEvent))}>{statusComponent}</li>
+  )
+}
 
 
 export default function Header() {
-
-  const dispatch = useAppDispatch();
 
   return(
     <HeaderStyled>
@@ -16,14 +38,13 @@ export default function Header() {
         <Image
           className="img-static"
           src={logo} alt="logo da empresa futurai"
-          width={246}
-          height={148}
+          width={190}
         />
       </h1>
       <nav>
         <ul>
-          <li onClick={() => dispatch(increment("processes"))}>Processos</li>
-          <li onClick={() => dispatch(increment("processForm"))}>Adicionar Processo</li>
+          <ItemNav dispatchEvent="processes" statusComponent="Processos"/>
+          <ItemNav dispatchEvent="processForm" statusComponent="Cadastrar processo"/>
         </ul>
       </nav>
     </HeaderStyled>
--- frontend/src/components/header/header.styled.ts
+++ frontend/src/components/header/header.styled.ts
@@ -3,7 +3,38 @@ import styled from "styled-components";
 
 
 export const HeaderStyled = styled.header`
+  display: flex;
+  align-items: flex-end;
+  background-color: #98c0d6;
+  box-shadow: 0 3px 4px rgba(0, 0, 0, 0.4);
+  min-height: 100px;
+  font-family: Skolarsanspe bd, sans-serif;
+
+  nav {
+    width: 100vw;
+  }
+
+  h1 {
+    padding: 1rem;
+    position: fixed;
+  }
+
+
+  ul {
+    display: flex;
+  }
+
   li {
+    list-style: none;
     cursor: pointer;
+    flex-basis: 50%;
+    text-align: center;
+    font-size: 25px;
+  }
+
+  .componentSeleted {
+    border-bottom: 3px solid #13afdb;
+    color: #13afdb;
+    font-weight: bold;
   }
 `;
