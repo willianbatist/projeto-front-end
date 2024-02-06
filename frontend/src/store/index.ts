873c0b0148443259c79b9ac33960e83235232e8a
Willian Batista <willian.alves.b15@gmail.com>
2024-02-06 18:48:32 -0300

    Update index.ts

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -9,4 +9,5 @@ export const store = configureStore({
   devTools: process.env.NODE_ENV !== "production",
 });
 
-
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
